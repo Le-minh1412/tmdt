6330b3312d341c2322f9a014f211660dab86c628
Lê Văn Minh <MinhLV.B22CN534@stu.ptit.edu.vn>
2026-05-30 17:30:34 +0700

    orders

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -1,28 +1,28 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv').config();
+const mysql = require("mysql2");
+const dotenv = require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+  host: process.env.MYSQLHOST || process.env.DATABASE_HOST,
+  user: process.env.MYSQLUSER || process.env.DATABASE_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DATABASE_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DATABASE,
+  port: process.env.MYSQLPORT || process.env.DATABASE_PORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log('Database connection failed:', err);
-    } else {
-        console.log('Connected database');
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("Connected database");
 
-        db.query("SET SESSION sql_mode = ''", (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('SQL MODE disabled');
-            }
-        });
-    }
+    db.query("SET SESSION sql_mode = ''", (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("SQL MODE disabled");
+      }
+    });
+  }
 });
 
 module.exports = db;
